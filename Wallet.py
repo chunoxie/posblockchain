e1a1baac6cd42e2cccc45fd0d95765fef6056220
chunoxie <michael.chunox@gmail.com>
2022-06-06 19:57:44 +0100

    traced issue of invalid signature between Wallet and Transaction

--- Wallet.py
+++ Wallet.py
@@ -11,3 +11,16 @@ class Wallet:
         signature_scheme_object = PKCS1_v1_5.new(self.key_pair)
         signature = signature_scheme_object.sign(data_hash)
         return signature.hex()
+
+    @staticmethod
+    def signature_valid(data, signature, public_key_string):
+        signature = bytes.fromhex(signature)
+        data_hash = BlockchainUtils.hash(data)
+        public_key = RSA.importKey(public_key_string)
+        signature_scheme_object = PKCS1_v1_5.new(public_key)
+        signature_valid = signature_scheme_object.verify(data_hash, signature)
+        return signature_valid
+
+    def public_key_string(self):
+        public_key_string = self.key_pair.publickey().exportKey('PEM').decode('utf-8')
+        return public_key_string
--- test_script.py
+++ test_script.py
@@ -16,8 +16,11 @@ def test_transaction():
     print("\nPrinting the to_json method:\n", transaction.to_json())
     print("\nPrinting signature from Wallet:\n", signature)
 
-    transaction.sign(signature)
+    # transaction.sign(signature)
     print("\nPrinting signed transaction the to_json method:\n", transaction.to_json())
+
+    signature_valid = Wallet.signature_valid(transaction.to_json(), signature, wallet.public_key_string())
+    print("\nTesting if signature is valid:\n", signature_valid)
 
 if __name__ == '__main__':
     test_transaction()
