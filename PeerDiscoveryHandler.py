74e8f778767f6097b2db3b3bfbf51723b0c7fb69
chunoxie <michael.chunox@gmail.com>
2022-06-15 12:28:05 +0100

    Added peer discovery class and updated socket communication.

---
+++ PeerDiscoveryHandler.py
@@ -0,0 +1,22 @@
+import threading, time
+
+class PeerDiscoveryHandler:
+    def __init__(self, node):
+        self.socket_communication = node
+
+    def start(self):
+        status_thread = threading.Thread(self.status, args=())
+        status_thread.start()
+        
+        discovery_thread = threading.Thread(self.discovery, args=())
+        discovery_thread.start()
+
+    def status(self):
+        while True:
+            print('status')
+            time.sleep(10)
+
+    def discovery(self):
+        while True:
+            print('discovery...')
+            time.sleep(10)
