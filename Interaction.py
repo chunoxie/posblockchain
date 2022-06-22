fa86511b90d56ad578ca1eb0278fe2d3dc636aa9
chunoxie <michael.chunox@gmail.com>
2022-06-22 12:27:19 +0100

    Minor refactor to simulate proof of stake blockchain prototype.

--- Interaction.py
+++ Interaction.py
@@ -1,19 +1,28 @@
+from Block import Block
 from Wallet import Wallet
 from utils import BlockchainUtils
 import requests
 
+def post_transaction(sender, receiver, amount, type):
+    transaction = sender.create_transaction(receiver.public_key_string(), amount, type)
+    url = 'http://localhost:5005/transaction'
+    package = {'transaction': BlockchainUtils.encode(transaction)}
+    request = requests.post(url, json=package)
+    print(request.text)
+
 def test_interaction():
     bob = Wallet()
     alice = Wallet()
+    alice.from_key('keys/stakerPrivateKey.pem')
     exchange = Wallet()
 
-    transaction = exchange.create_transaction(alice.public_key_string(), 10, 'EXCHANGE')
+    # forger: genesis
+    post_transaction(exchange, alice, 100, 'EXCHANGE')
+    post_transaction(exchange, bob, 100, 'EXCHANGE')
+    post_transaction(alice, alice, 25, 'STAKE')
 
-    url = 'http://localhost:5005/transaction'
-    package = {'transaction': BlockchainUtils.encode(transaction)}
-
-    request = requests.post(url, json=package)
-    print(request.text)
+    # forger: most likely Alice
+    post_transaction(alice, bob, 1, 'TRANSFER')
 
 
 if __name__ == '__main__':
